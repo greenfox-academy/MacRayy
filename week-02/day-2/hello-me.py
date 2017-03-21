0e3c0b91b256a038625bccb3a49aab586e8e72ff
Zsolt Makrai <mecrai@gmail.com>
2017-03-21 11:25:42 +0100

    print in python

---
+++ week-02/day-2/hello-me.py
@@ -0,0 +1,10 @@
+# Greet 3 of your classmates with this program, in three separate lines
+# like:
+#
+# Hello, Esther!
+# Hello, Mary!
+# Hello, Joe!
+
+print("Hello Balazs")
+print("Hello Dora")
+print("Hello Pati")
