ee2e7a9d8ee2b6169135b4e9951e41a935ad5086
Zsolt Makrai <mecrai@gmail.com>
2017-03-21 12:01:43 +0100

    basic math

---
+++ week-02/day-2/two-numbers.py
@@ -0,0 +1,36 @@
+# Create a program that prints a few operations on two numbers: 22 and 13
+
+# Print the result of 13 added to 22
+
+# Print the result of 13 substracted from 22
+
+# Print the result of 22 mutiplied by 13
+
+# Print the result of 22 divided by 13 (as a decimal fraction)
+
+# Print the integer part of 22 divided by 13
+
+# Print the reminder of 22 divided by 13
+
+firstop = 13 + 22
+print(firstop)
+
+secondop = 22 - 13
+print(secondop)
+
+thirdop = 22 * 13
+print(thirdop)
+
+fourthop = 22 / 13
+print(fourthop)
+
+fifthop = 22 // 13
+print(fifthop)
+
+sixthop = 22 % 13
+print(sixthop)
+
+seventhop = 22 ** 13
+print(seventhop)
+
+print(22 + 13)
