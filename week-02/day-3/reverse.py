086299103fe673c4ac2ff40d2046372585053a7e
Zsolt Makrai <mecrai@gmail.com>
2017-03-22 15:23:09 +0100

    create some more lists

---
+++ week-02/day-3/reverse.py
@@ -0,0 +1,10 @@
+# - Create a variable named `aj`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Reverse the order of the elements in `aj`
+# - Print the elements of the reversed `aj`
+
+aj = [3, 4, 5, 6, 7]
+
+aj[:] = aj[4], aj[3], aj[2], aj[1], aj[0]
+
+print(aj)
