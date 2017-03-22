83743ebbc85992d94be100266d5909e71cd2455b
Zsolt Makrai <mecrai@gmail.com>
2017-03-22 22:18:03 +0100

    awesome fizzbuzz done with a function

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Bajusz - greenfox exercises
 [Zodiac Cohort syllabus](https://github.com/greenfox-academy/zodiac-syllabus)
 
 Links to projects and repositories:
-You can see the [First weekly exercise - CV](https://macrayy.github.io/) and its [ repo](https://www.google.com "Google's Homepage")
+You can see the [First weekly exercise - CV](https://macrayy.github.io/) and its [ repo](https://github.com/MacRayy/macrayy.github.io)
 
 [Command line and git lesson](https://github.com/MacRayy/git-lesson-repository)
---
+++ week-02/day-2/buzzfizz.py
@@ -0,0 +1,18 @@
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the number
+# and for the multiples of five print “Buzz”.
+# For numbers which are multiples of both three and five print “FizzBuzz”.
+
+def numbers():
+    for n in range(100):
+        n = n + 1
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        elif n % 5 == 0:
+            print("Buzz")
+        else:
+            print(n)
+
+numbers()
