d6a39f23b7b560f5ed8665740d333b286093240c
Zsolt Makrai <mecrai@gmail.com>
2017-03-23 17:30:48 +0100

    working matchmaker

---
+++ week-02/day-4/matchmaking.py
@@ -0,0 +1,8 @@
+# Join the two lists by matching one girl with one boy in the order list
+# Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+girls = ["Eve","Ashley","Bozsi","Kat","Jane"]
+boys = ["Joe","Fred","Bela","Todd","Neef","Jeff"]
+order = [girls[0] + ", " + boys[0] + ", " + girls[1] + ", " + boys[1] + ", " + girls[2] + ", " + boys[2]]
+
+print(order)
