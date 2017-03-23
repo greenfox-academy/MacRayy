98cb14df14cbe575004be363be7d77ff2f139268
Zsolt Makrai <mecrai@gmail.com>
2017-03-23 23:42:46 +0100

    (Feat): clean start added

--- week-02/day-2/parametric-average.py
+++ week-02/day-2/parametric-average.py
@@ -1,11 +1,12 @@
 """
-// Write a program that asks for a number.
-// It would ask this many times to enter an integer,
-// if all the integers are entered, it should print the sum and average of these
-// integers like:
-//
-// Sum: 22, Average: 4.4
+ Write a program that asks for a number. It would ask this many times to enter an integer, if all the integers are entered, it should print the sum and average of these integers like:
+
+Sum: 22, Average: 4.4
 """
+# SUM AND AVERAGE COUNTER
+
+import os
+os.system("cls" if os.name == "nt" else "clear")
 
 while True:
     try:
@@ -23,8 +24,7 @@ def sum_and_ave():
     for x in range(how_many_numbers):
         user_number = user_number + float(input("Enter a number: "))
     Average = user_number / how_many_numbers
-
-print("Sum of the entered numbers: " + str(user_number))
-print("Average of the entered numbers: " + str(Average))
+    print("Sum of the entered numbers: " + str(user_number))
+    print("Average of the entered numbers: " + str(Average))
 
 sum_and_ave()
