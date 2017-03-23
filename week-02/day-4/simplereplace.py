9d58ec61352b64feacb1eac95e7418216a714c7b
Zsolt Makrai <mecrai@gmail.com>
2017-03-23 13:17:40 +0100

    add alternate solution

--- week-02/day-4/simplereplace.py
+++ week-02/day-4/simplereplace.py
@@ -4,4 +4,8 @@ example = "In a dishwasher far far away"
 # Please fix it for me!
 # Expected ouput: In a galaxy far far away
 
+# not recommended solution
 print(example.replace("dishwasher", "galaxy"))
+
+# recommended solution
+print(example[:5] + "galaxy" + example[15:])
