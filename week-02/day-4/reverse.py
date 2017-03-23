cbe8ece6e53d42796ed5858900454d462af434c2
Zsolt Makrai <mecrai@gmail.com>
2017-03-23 17:16:42 +0100

    reverser is operational

---
+++ week-02/day-4/reverse.py
@@ -0,0 +1,9 @@
+# Reverse the string using StringBuilder
+
+reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+def reverser(rev):
+    normal_sentance = reversed[::-1]
+    print(normal_sentance)
+
+reverser(reversed)
