772ff73af25f30fab04168defa8406aebb64a5aa
Zsolt Makrai <mecrai@gmail.com>
2017-03-23 16:35:36 +0100

    candie counter is ready

---
+++ week-02/day-4/student-counter.py
@@ -0,0 +1,28 @@
+"""students = [
+
+
+]
+"""
+# create a function that takes a list of students and prints:
+# - how many candies are owned by students
+
+# create a function that takes a list of students and prints:
+# - Sum of the age of people who have lass than 5 candies
+
+students = [{'name': 'Teodor', 'age': 3, 'candies': 2},
+{'name': 'Rezso', 'age': 9.5, 'candies': 2},
+{'name': 'Zsombor', 'age': 12, 'candies': 5},
+{'name': 'Aurel', 'age': 7, 'candies': 3},
+{'name': 'Olaf', 'age': 12, 'candies': 7},
+{'name': 'Gerzson', 'age': 10, 'candies': 1}]
+
+for student in students:
+    print(student['name'])
+
+def candie_counter(candie):
+    total_candies = 0
+    for student in students:
+        total_candies += student['candies']
+    return total_candies
+
+print(candie_counter(students))
