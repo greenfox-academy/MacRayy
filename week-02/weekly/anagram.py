480c76e1d816535d0a70622432c4658263b46301
Zsolt Makrai <mecrai@gmail.com>
2017-03-24 00:07:27 +0100

    Have a structure for the program

---
+++ week-02/weekly/anagram.py
@@ -0,0 +1,13 @@
+# ANAGRAM
+
+# Create a function named is anagram following your current language's style guide. It should take two strings and return a boolean value depending on whether its an anagram or not.
+
+# Input UI
+
+input_1 = "dog" #input("word")
+input_2 = "god" #input("word")
+
+# Function that compares the input words
+
+
+# Display true or false
