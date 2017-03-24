4c11b1d3751974e6a243c760fc7952f3ad4bfa44
Zsolt Makrai <mecrai@gmail.com>
2017-03-24 11:19:14 +0100

    add comments

--- week-02/weekly/palindrome-builder.py
+++ week-02/weekly/palindrome-builder.py
@@ -1,5 +1,8 @@
+# PALINDROME BILDER
+
 # Create a function named create palindrome following your current language's style guide. It should take a string, create a palindrome from it and then return it.
 
+# UI
 import os
 os.system("cls" if os.name == "nt" else "clear")
 
@@ -7,9 +10,11 @@ print("Welcome, I am Pali, the Palindrome Creator!")
 
 word = input("Please enter a word and I will creat a palindrome from it: ")
 
+# Function the bauilds the palindrome
 def create_palindrome(palindrome):
     dorw = word[::-1]
     palindrome = word + dorw
     return palindrome
 
+# Display
 print(create_palindrome(word))
