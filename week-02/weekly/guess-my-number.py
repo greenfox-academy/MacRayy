64fec7104cfdf38faba79221879266c2a529a011
Zsolt Makrai <mecrai@gmail.com>
2017-03-24 11:47:14 +0100

    add comments

--- week-02/weekly/guess-my-number.py
+++ week-02/weekly/guess-my-number.py
@@ -1,8 +1,10 @@
-#Write a program where the program chooses a number between 1 and 100. The player is then asked to enter a guess. If the player guesses wrong, then the program gives feedback and ask to enter an other guess until the guess is correct.
+# GUESS MY NUMBER
 
+#Write a program where the program chooses a number between 1 and 100. The player is then asked to enter a guess. If the player guesses wrong, then the program gives feedback and ask to enter an other guess until the guess is correct.
 #Make the range customizable (ask for it before starting the guessing).
 #You can add lives. (optional)
 
+# UI
 import os
 os.system("cls" if os.name == "nt" else "clear")
 from random import randint
@@ -14,6 +16,7 @@ print("Well, " + user + " let's play! \n But first please tell me the range wher
 range_start = float(input("From: "))
 range_end = float(input("To: "))
 
+# Loop that checks the input numbers
 while True:
     if range_start > range_end:
         print("Pick a bigger number!")
@@ -25,8 +28,8 @@ while True:
     elif range_start < range_end:
         break
 
+# Game function
 random_num = (randint(range_start, range_end))
-
 def guess_the_number(random_num):
     number = int(input("Can you guess my number? "))
     while number != random_num:
