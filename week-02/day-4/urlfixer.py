133e60c3699463bfa095eb27890d24878da60d00
Zsolt Makrai <mecrai@gmail.com>
2017-03-26 12:18:38 +0200

    (Fix):url fixed

---
+++ week-02/day-4/urlfixer.py
@@ -0,0 +1,7 @@
+# Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+# Also, the URL is missing a crutial component, find out what it is and insert it too!
+# https://www.reddit.com/r/nevertellmetheodds/
+
+url = "https//www.reddit.com/r/nevertellmethebots"
+
+print(url[:5] + ":" +  url[5:-4] + "odds/")
