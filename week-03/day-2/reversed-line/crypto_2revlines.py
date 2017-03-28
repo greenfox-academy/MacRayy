64bc33927f8ca2dad441cfdc4e859da04358dd89
Zsolt Makrai <mecrai@gmail.com>
2017-03-28 13:56:58 +0200

    two working versions

--- week-03/day-2/reversed-line/crypto_2revlines.py
+++ week-03/day-2/reversed-line/crypto_2revlines.py
@@ -1,12 +1,27 @@
 # Create a method that decrypts texts/reversed_zen_lines.txt
 
+# one version
+'''
 def decrypt(file_name):
     f = open(file_name, "r")
     text = f.readlines()
     readalble_text = ""
     for i in text:
         readalble_text += i[::-1]
-    print(readalble_text)
+    f.close()
+    return readalble_text
+
+
+print(decrypt("reversed-lines.txt"))
+'''
+
+# second version
+def decrypt(file_name):
+    f = open(file_name, "r")
+    text = ""
+    for line in f:
+        text += line.rstrip()[::-1] + '\n'
+    print(text)
     f.close()
 
 decrypt("reversed-lines.txt")
