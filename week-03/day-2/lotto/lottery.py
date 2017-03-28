e48fb80b993dc5312692ad24dab35a1feeb4afc2
Zsolt Makrai <mecrai@gmail.com>
2017-03-28 22:20:48 +0200

    feat(lists all the numbers)

--- week-03/day-2/lotto/lottery.py
+++ week-03/day-2/lotto/lottery.py
@@ -1,10 +1,13 @@
 # Create a method that returns the five most frequent lottery number in a pretty table format
+
 def five_most_frequent(file_name):
     f = open(file_name, "r")
     numbers = ""
+    last_numbers = []
     for line in f:
-        numbers += line.rstrip() + '\n'
+        numbers = line.rstrip().split(";")
+        last_numbers += numbers[11:16]
     f.close()
-    return numbers
+    return last_numbers
 
 print(five_most_frequent("otos sample.csv"))
