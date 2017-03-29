71c6a3727da2bdd8dc3d65ac1492fdb761120073
Zsolt Makrai <mecrai@gmail.com>
2017-03-29 11:58:03 +0200

    feat(colors of the rainbow in boxes)

---
+++ week-03/day-3/rainbow-box-function.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# create a square drawing function that takes 2 parameters:
+# the square size, and the fill color,
+# and draws a square of that size and color to the center of the canvas.
+# create a loop that fills the canvas with rainbow colored squares.
+rainbowcolors = ["red", "orange", "yellow", "green", "blue", "purple"]
+size = 200
+
+def draw_square(size, color):
+    for color in rainbowcolors:
+        size -= 30
+        square = canvas.create_rectangle((150-size/2,150-size/2, 150+size/2,150+size/2), fill=color)
+
+draw_square(size, rainbowcolors)
+
+root.mainloop()
