2d0972ad652ea655f4db1c4b677a9076927f88a6
Zsolt Makrai <mecrai@gmail.com>
2017-03-29 11:40:54 +0200

    feat(boxis in the center)

--- week-03/day-3/center-box-function.py
+++ week-03/day-3/center-box-function.py
@@ -10,7 +10,7 @@ canvas.pack()
 # and draws a square of that size to the center of the canvas.
 # draw 3 squares with that function.
 def draw_square(size):
-    square = canvas.create_rectangle(50,50, 50+size,50+size, fill="pink")
+    square = canvas.create_rectangle(150-size/2,150-size/2, 150+size/2,150+size/2, fill="pink")
 
 draw_square(200)
 draw_square(100)
