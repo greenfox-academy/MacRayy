dd81e2c79094a0596b752a3cdfd720517da424dd
Zsolt Makrai <mecrai@gmail.com>
2017-03-29 10:58:17 +0200

    feat(function draw three lines)

---
+++ week-03/day-3/horizontal-lines.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+# create a line drawing function that takes 2 parameters:
+# the x and y coordinates of the line's starting point
+# and draws a 50 long horizontal line from that point.
+# draw 3 lines with that function.
+def draw_lines(x, y):
+    line = canvas.create_line(x,y, x+50,y)
+
+draw_lines(10,10)
+draw_lines(50,50)
+draw_lines(100,100)
+
+root.mainloop()
