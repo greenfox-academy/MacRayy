13d6b61d72b265bbdb478ed4b099620409069105
Zsolt Makrai <mecrai@gmail.com>
2017-03-29 17:27:08 +0200

    fix(nicer code)

--- week-03/day-3/function-to-center.py
+++ week-03/day-3/function-to-center.py
@@ -10,18 +10,11 @@ canvas.pack()
 # and draws a line from that point to the center of the canvas.
 # fill the canvas with lines from the edges, every 20 px, to the center.
 def draw_lines(x, y):
-    for i in range(0, 301, 20):
-        line_1 = canvas.create_line(x,y, 150,150)
-        x += 20
-    for i in range(0, 301, 20):
-        line_2 = canvas.create_line(x,y, 150,150)
-        y += 20
-    for i in range(0, 301, 20):
-        line_3 = canvas.create_line(x,y, 150,150)
-        x -= 20
-    for i in range(0, 301, 20):
-        line_4 = canvas.create_line(x,y, 150,150)
-        y -= 20
+    for i in range(16):
+        line_1 = canvas.create_line(i*20,y, 150,150)
+        line_2 = canvas.create_line(x,i*20, 150,150)
+        line_3 = canvas.create_line(i*20,y+300, 150,150)
+        line_1 = canvas.create_line(x+300,i*20, 150,150)
 
 draw_lines(0,0)
 
