755e91d13dfc47bbfbfb9ea702df2db52dc97e0f
Zsolt Makrai <mecrai@gmail.com>
2017-03-30 21:29:07 +0200

    feat(square fractal): draw fractal squares in wron place

--- week-03/day-4/yellow.py
+++ week-03/day-4/yellow.py
@@ -6,11 +6,16 @@ canvas = Canvas(root, width='600', height='600')
 canvas.pack()
 
 def draw_square(x,y, size):
-    for i in range(7):
-        square = canvas.create_rectangle(x,y, x+size,y+size)
+    square = canvas.create_rectangle(x,y, x+size,y+size, fill="yellow")
 
+def recursive_squares(x,y, size):
+    draw_square(x,y, size)
+    if size > 5:
+        recursive_squares(x+size/3,y, size/3)
+        #recursive_squares(x+2/3*size,y, size/3)
+        #recursive_squares(x,y+2/3*size, size/3)
+        #recursive_squares(x+2/3*size,y+2/3*size, size/3)
 
-
-draw_square(10,10, 580)
+recursive_squares(10,10, 580)
 
 root.mainloop()
