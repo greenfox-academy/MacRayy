0a4b9702ba89081b753d224258cc540a730aaf63
Zsolt Makrai <mecrai@gmail.com>
2017-03-31 11:07:04 +0200

    feat(kochline): draws one part of the line

--- week-03/weekly/kochline.py
+++ week-03/weekly/kochline.py
@@ -5,14 +5,14 @@ root = Tk()
 canvas = Canvas(root, width='600', height='600')
 canvas.pack()
 
-def draw_line(x,y):
-    line = canvas.create_line(x,y, x*58,y, fill="blue")
+def draw_line(x,y, lenght):
+    line = canvas.create_line(x,y, lenght/3,y, lenght/2,y/1.5, 2*(lenght/3),y, lenght,y, fill="blue")
 
 """
-def recursive_lines(x,y):
-    draw_hexagon(x,y)
+def recursive_lines(x,y, lenght):
+    draw_line(x,y, lenght)
 """
 
-draw_line(10, 400)
+draw_line(10,400, 580)
 
 root.mainloop()
