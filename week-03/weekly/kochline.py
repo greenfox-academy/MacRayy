df1bcbc56e80d1f76bd5aa2e3bae4e0f5462bbcd
Zsolt Makrai <mecrai@gmail.com>
2017-03-31 11:10:41 +0200

    feat(kochline): recursion not workig

--- week-03/weekly/kochline.py
+++ week-03/weekly/kochline.py
@@ -8,11 +8,13 @@ canvas.pack()
 def draw_line(x,y, lenght):
     line = canvas.create_line(x,y, lenght/3,y, lenght/2,y/1.5, 2*(lenght/3),y, lenght,y, fill="blue")
 
-"""
+
 def recursive_lines(x,y, lenght):
     draw_line(x,y, lenght)
-"""
+    if lenght > 50:
+        draw_line(x,y, lenght*3**0.5)
 
-draw_line(10,400, 580)
+
+recursive_lines(10,400, 580)
 
 root.mainloop()
