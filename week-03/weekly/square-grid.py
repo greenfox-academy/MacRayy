83e5d97281549f52da51eb98d54860854e6ddf5b
Zsolt Makrai <mecrai@gmail.com>
2017-03-31 14:20:22 +0200

    feat(fractal grid): ls

--- week-03/weekly/square-grid.py
+++ week-03/weekly/square-grid.py
@@ -2,20 +2,20 @@ from tkinter import *
 
 root = Tk()
 
-canvas = Canvas(root, width='600', height='600')
+canvas = Canvas(root, width='600', height='600', bg="#e8ed61")
 canvas.pack()
 
 def draw_square(x,y, size, width):
     square = canvas.create_rectangle(x,y, x+size,y+size, outline="darkblue", width=width)
-'''
+
 def recursive_squares(x,y, size, width):
     draw_square(x,y, size, width)
-    if size > 5:
-        recursive_squares(x+size/3,y, size/3, "width")
-        recursive_squares(x,y+size/3, size/3, "w")
-        recursive_squares(x+2*size/3,y+size/3, size/3, "width")
-        recursive_squares(x+size/3,y+2*size/3, size/3, "width")
-'''
-draw_square(10,10, 580, 10)
+    if width > 1:
+        recursive_squares(x-size/4,y-size/4, size/2, width/2)
+        recursive_squares(x-size/4,y+size*0.75, size/2, width/2)
+        recursive_squares(x+size*0.75,y-size/4, size/2, width/2)
+        recursive_squares(x+size*0.75,y+size*0.75, size/2, width/2)
+
+recursive_squares(180,180, 250, 20)
 
 root.mainloop()
