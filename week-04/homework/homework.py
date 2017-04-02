e32d1a48f30861d57f5ccd6316fca0fa6a16d56e
Zsolt Makrai <mecrai@gmail.com>
2017-04-02 15:50:16 +0200

    trees in list

--- week-04/homework/homework.py
+++ week-04/homework/homework.py
@@ -7,15 +7,12 @@ age
 sex
 you can use just variables, or lists and/or maps
 """
+trees = [
+    {"type": "oak", "leaf_color": "green", "age": 2, "sex": "male"},
+    {"type": "pine", "leaf_color": "green", "age": 3, "sex": "female"},
+    {"type": "apple", "leaf_color": "green", "age": 1, "sex": "male"},
+    {"type": "momiji", "leaf_color": "red", "age": 6, "sex": "fmale"},
+    {"type": "birch", "leaf_color": "green", "age": 5, "sex": "male"}]
 
-tree_1 = {"type": "oak", "leaf_color": "green", "age": 2, "sex": "male"}
-
-tree_2 = {"type": "pine", "leaf_color": "green", "age": 3, "sex": "female"}
-
-tree_3 = {"type": "apple", "leaf_color": "green", "age": 1, "sex": "male"}
-
-tree_4 = {"type": "momiji", "leaf_color": "red", "age": 6, "sex": "fmale"}
-
-tree_5 = {"type": "birch", "leaf_color": "green", "age": 5, "sex": "male"}
-
-print(tree_1[type])
+for tree in trees:
+    print(tree["leaf_color"])
