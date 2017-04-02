caa32cebba796a70e5b08f58c0abf73f36b221d1
Zsolt Makrai <mecrai@gmail.com>
2017-04-02 15:41:45 +0200

    init trees

---
+++ week-04/homework/homework.py
@@ -0,0 +1,21 @@
+"""Create 5 trees
+Store the data of them in variables in your program
+for every tree the program should store its'
+type
+leaf color
+age
+sex
+you can use just variables, or lists and/or maps
+"""
+
+tree_1 = {"type": "oak", "leaf_color": "green", "age": 2, "sex": "male"}
+
+tree_2 = {"type": "pine", "leaf_color": "green", "age": 3, "sex": "female"}
+
+tree_3 = {"type": "apple", "leaf_color": "green", "age": 1, "sex": "male"}
+
+tree_4 = {"type": "momiji", "leaf_color": "red", "age": 6, "sex": "fmale"}
+
+tree_5 = {"type": "birch", "leaf_color": "green", "age": 5, "sex": "male"}
+
+print(tree_1[type])
