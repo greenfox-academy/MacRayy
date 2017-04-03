4a91f5ee35856a6f5ba1c2cf8c575fafadeb2ba5
Zsolt Makrai <mecrai@gmail.com>
2017-04-03 11:35:46 +0200

    refactor(domino): good order

--- week-04/day-1/dominoes.py
+++ week-04/day-1/dominoes.py
@@ -3,11 +3,11 @@ from domino import Domino
 def initialize_dominoes():
     dominoes = []
     dominoes.append(Domino(5, 2))
-    dominoes.append(Domino(4, 6))
-    dominoes.append(Domino(1, 5))
-    dominoes.append(Domino(6, 7))
     dominoes.append(Domino(2 ,4))
+    dominoes.append(Domino(4, 6))
+    dominoes.append(Domino(6, 7))
     dominoes.append(Domino(7, 1))
+    dominoes.append(Domino(1, 5))
     return dominoes
 
 dominoes = initialize_dominoes()
