36ddcd8d11a796a519187c742f07b771fea7baed
Zsolt Makrai <mecrai@gmail.com>
2017-04-03 11:40:24 +0200

    init dice

---
+++ week-04/day-1/dice.py
@@ -0,0 +1,32 @@
+import random
+
+class Dice():
+
+    def __init__(self):
+        self.dice = [0, 0, 0, 0, 0, 0]
+
+    def roll(self):
+        for i in range(len(self.dice)):
+            self.dice[i] = random.randint(1,6)
+        return self.dice
+
+    def getCurrent(self, index=None):
+        if index != None:
+            return self.dice[index]
+        else:
+            return self.dice
+
+    def reroll(self, index=None):
+        if index != None:
+            self.dice[index] = random.randint(1,6)
+        else:
+            self.roll()
+
+
+dice = Dice()
+print(dice.getCurrent())
+dice.roll()
+print(dice.getCurrent())
+dice.reroll(3)
+print(dice.getCurrent(3))
+print(dice.getCurrent())
