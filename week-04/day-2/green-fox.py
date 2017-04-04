bf2ee23a9d984c07fd5d878cd75f5cf3fe0656bc
Zsolt Makrai <mecrai@gmail.com>
2017-04-04 13:21:38 +0200

    fix(greenfox): bugs fixed, tested

--- week-04/day-2/green-fox.py
+++ week-04/day-2/green-fox.py
@@ -1,26 +1,26 @@
 class Person(object):
-    def __init__(self, name, age, gender):
+    def __init__(self, name = "Jane Doe", age = 0, gender = "female"):
         self.name = name
         self.age = age
         self.gender = gender
 
     def introduce(self):
-        return "Hi, I'm " +  self.name + ", a " + str(self.age) + " year old " + self.gender + "."
+        print( "Hi, I'm " +  self.name + ", a " + str(self.age) + " year old " + self.gender + ".")
 
     def get_goal(self):
-        return "My goal is: Live for the moment!"
+        print( "My goal is: Live for the moment!")
 
 class Student(Person):
-    def __init__(self, name, age, gender, previous_organization, skipped_days = 0):
+    def __init__(self, name = "Jane Doe", age = 0, gender = "female", previous_organization = "Life", skipped_days = 0):
         super().__init__(name, age, gender)
         self.previous_organization = previous_organization
         self.skipped_days = skipped_days
 
     def get_goal(self):
-        return "Be a junior software developer!"
+        print( "Be a junior software developer!")
 
     def introduce(self):
-        return "Hi, I'm " +  self.name + ", a " + str(self.age) + " year old " + self.gender + " from " + self.previous_organization + " who skipped " +  str(self.skipped_days) + " days from the course already."
+        print( "Hi, I'm " +  self.name + ", a " + str(self.age) + " year old " + self.gender + " from " + self.previous_organization + " who skipped " +  str(self.skipped_days) + " days from the course already.")
 
     def skip_days(self, number_of_days):
         self.number_of_days = number_of_days
@@ -28,33 +28,79 @@ class Student(Person):
         return self.skipped_days
 
 class Mentor(Person):
-    def __init__(self, name, age, gender, level = "junior"):
+    def __init__(self, name = "Jane Doe", age = 0, gender = "female", level = "junior"):
         super().__init__(name, age, gender)
         self.level = level
 
     def get_goal(self):
-        return "Educate brilliant junior software developers."
+        print( "Educate brilliant junior software developers.")
 
     def introduce(self):
-        return "Hi, I'm " +  self.name + ", a " + str(self.age) + " year old " + self.gender + " " + self.level + " mentor."
+        print( "Hi, I'm " +  self.name + ", a " + str(self.age) + " year old " + self.gender + " " + self.level + " mentor.")
 
 class Sponsor(Person):
-    def __init__(self, name, age, gender, company, hired_students = 0):
+    def __init__(self, name = "Jane Doe", age = 0, gender = "female", company = "Google", hired_students = 0):
         super().__init__(name, age, gender)
         self.company = company
         self.hired_students = hired_students
 
     def get_goal(self):
-        return "Hire brilliant junior software developers."
+        print( "Hire brilliant junior software developers.")
 
     def introduce(self):
-        return "Hi, I'm " +  self.name + ", a " + str(self.age) + " year old " + self.gender + " who represents " + self.company + " and hired " + str(self.hired_students) + " students so far."
+        print( "Hi, I'm " +  self.name + ", a " + str(self.age) + " year old " + self.gender + " who represents " + self.company + " and hired " + str(self.hired_students) + " students so far.")
 
     def hire(self):
         self.hired_students += 1
         return self.hired_students
 
-person_1 = Sponsor("Jane Doe", 30, "female", "Google", 0)
+class LagopusClass():
+    def __init__(self, class_name = "zodiac", students = 0, mentors = 0):
+        self.class_name = class_name
+        self.students = []
+        self.mentors = []
 
-person_1.hire()
-print(person_1.introduce())
+    def add_student(self, student):
+        self.students.append(student)
+
+    def add_mentor(self, mentor):
+        self.mentors.append(mentor)
+
+    def info(self):
+        print( "Lagopus " + self.class_name + " class has " + str(len(self.students)) + " students and " + str(len(self.mentors)) + " mentors.")
+
+people = []
+
+mark = Person('Mark', 46, 'male')
+people.append(mark)
+jane = Person()
+people.append(jane)
+john = Student('John Doe', 20, 'male', 'BME')
+people.append(john)
+student = Student()
+people.append(student)
+gandhi = Mentor('Gandhi', 148, 'male', 'senior')
+people.append(gandhi)
+mentor = Mentor()
+people.append(mentor)
+sponsor = Sponsor()
+elon = Sponsor('Elon Musk', 46, 'male', 'SpaceX')
+people.append(elon)
+student.skip_days(3)
+
+for i in range(5):
+    elon.hire()
+
+for i in range(3):
+    sponsor.hire()
+
+for member in people:
+    member.introduce()
+    member.get_goal()
+
+badass = LagopusClass('BADA55')
+badass.add_student(student);
+badass.add_student(john);
+badass.add_mentor(mentor);
+badass.add_mentor(gandhi);
+badass.info();
