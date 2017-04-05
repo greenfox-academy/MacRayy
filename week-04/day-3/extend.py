1e1ca7c6561b062bac537f25b3b318dc0f20e7c3
Zsolt Makrai <mecrai@gmail.com>
2017-04-05 19:27:49 +0200

    fix(max of three): working and tested

--- week-04/day-3/extend.py
+++ week-04/day-3/extend.py
@@ -4,8 +4,10 @@ def add(a, b):
 
 # Returns the highest value from the three given params
 def max_of_three(a, b, c):
-    if a > b:
+    if a > b and a > c:
         return a
+    elif b > a and b > c:
+        return b
     else:
         return c
 
