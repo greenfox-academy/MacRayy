d070aa8fb1777a38177631aa1d67f6267e1fe1c2
Zsolt Makrai <mecrai@gmail.com>
2017-04-10 22:38:31 +0200

    feat(aircraft): working aircraft classes and methods

--- week-04/day-2/aircraft_carrier.py
+++ week-04/day-2/aircraft_carrier.py
@@ -1,27 +1,56 @@
 #AIRCRAFT CARRIER
 
 class Aircraft(object):
-    def __init__(self, max_ammo, base_damage, ammo = 0):
+    def __init__(self, plane_type, max_ammo = 0, base_damage = 0, ammo = 0):
         self.ammo = ammo
         self.max_ammo = max_ammo
         self.base_damage = base_damage
+        self.plane_type = plane_type
 
     def figth(self):
-        pass
+        damage = self.ammo * self.base_damage
+        self.ammo = 0
+        return damage
 
-    def refill(self):
-        pass
+    def refill(self, fill_ammo):
+        if fill_ammo < self.max_ammo:
+            self.ammo += fill_ammo
+            fill_ammo -= self.ammo
+            return fill_ammo
+        else:
+            self.ammo = self.max_ammo
+            fill_ammo -= self.max_ammo
+            return fill_ammo
 
     def get_type(self):
-        pass
+        return self.plane_type
 
     def get_status(self):
-        pass
+        damage = self.figth()
+        status = "Type " + self.plane_type + ", Ammo: " + str(self.ammo) + " Base Damage: " + str(self.base_damage) + ", All Damage: " + str(damage)
+        return status
 
 class F16(Aircraft):
-    def __init__(self):
-        super().__init__(max_ammo, base_damage, ammo)
+    def __init__(self, plane_type, max_ammo, base_damage, ammo = 0):
+        super().__init__(plane_type, ammo)
+        self.max_ammo = 8
+        self.base_damage = 30
 
 class F35(Aircraft):
-    def __init__(self):
-        super().__init__(max_ammo, base_damage, ammo)
+    def __init__(self, plane_type, max_ammo, base_damage, ammo = 0):
+        super().__init__(plane_type, ammo)
+        self.max_ammo = 12
+        self.base_damage = 50
+
+aircraft_1 = F16("F16", 8, 30)
+aircraft_2 = F35("F35", 12, 50)
+
+print(aircraft_1.refill(200))
+print("\n")
+print(aircraft_1.figth())
+print("\n")
+print(aircraft_1.refill(100))
+print("\n")
+print(aircraft_1.get_type())
+print("\n")
+print(aircraft_1.get_status())
