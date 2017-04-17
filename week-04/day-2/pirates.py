2df948dbefc43687204479db8b7d375d162fb9a4
Zsolt Makrai <mecrai@gmail.com>
2017-04-17 23:06:40 +0200

    feat(pirates): battle method installed

--- week-04/day-2/pirates.py
+++ week-04/day-2/pirates.py
@@ -6,13 +6,14 @@ class Pirate(object):
         self.alive = True
         self.passed_out = False
 
-    def drink_some_rum(self):
-        self.toxic_o_mater += 1
+    def drink_some_rum(self, bottles):
+        self.toxic_o_mater += bottles
         return self.toxic_o_mater
 
     def hows_it_going_mate(self):
         if self.toxic_o_mater < 4:
             print("Aye lad, Pour me anudder!")
+            self.passed_out = False
             self.drink_some_rum()
             return self.toxic_o_mater
         else:
@@ -45,6 +46,7 @@ class Ship(object):
         self.name = name
         self.crew = []
         self.captain = None
+        self.alive_crew = []
 
     def fill_ship(self, pirate):
         if self.captain == None:
@@ -55,17 +57,37 @@ class Ship(object):
             pirate = Pirate()
             self.crew.append(pirate)
 
+    def remove_dead_mates(self):
+        for pirate in self.crew:
+            if pirate.alive:
+                self.alive_crew.append(pirate)
+        return self.alive_crew
+
     def get_ship_status(self):
         if self.captain.alive:
             print("Captain's consumed " + str(self.captain.toxic_o_mater) + " bottles of rum, and Passed out: " + str(self.captain.passed_out))
         else:
             print("Captain is dead")
-        print("Alive crew members:")
-        alive_crew = []
-        for pirate in self.crew:
-            if pirate.alive:
-                alive_crew.append(pirate)
-        print(len(alive_crew))
+        print("Alive crew members: " + str(len(self.remove_dead_mates())))
+
+    def battle(self, other_ship):
+        other_ship = Ship(other_ship)
+        ship_score = len(self.remove_dead_mates()) - self.captain.toxic_o_mater
+        other_ship_score = len(other_ship.remove_dead_mates()) - other_ship.captain.toxic_o_mater
+        if ship_score > other_ship_score:
+            for pirate in self.alive_crew:
+                pirate.drink_some_rum(randint(1, 5))
+            for enemy_pirate in other_ship.alive_crew[::(randint(1, len(other_ship.alive_crew)))]:
+                enemy_pirate.die()
+            return True
+        elif ship_score < other_ship_score:
+            for enemy_pirate in other_ship.alive_crew:
+                enemy_pirate.drink_some_rum(randint(1, 5))
+            for pirate in self.alive_crew[::(randint(1, len(self.alive_crew)))]:
+                pirate.die()
+            return False
+
+
 
 
 
