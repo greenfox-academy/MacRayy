a3ac40a1a44b0d6557fd3e825ad270d529d4e9e5
Zsolt Makrai <mecrai@gmail.com>
2017-05-02 13:19:01 +0200

    feat(ex_1): hello mates

---
+++ week-07/ex_2.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+function greetings() {
+    console.log('Hello Gabor');
+    console.log('Hello Bence');
+    console.log('Hello Simon');
+}
+
+greetings();
