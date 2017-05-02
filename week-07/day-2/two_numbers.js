ca6bf0b6847697af5731526af67dced93b7ce82f
Zsolt Makrai <mecrai@gmail.com>
2017-05-02 13:40:51 +0200

    feat(ex_5: operations in function

---
+++ week-07/day-2/two_numbers.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+
+// Print the result of 13 substracted from 22
+
+// Print the result of 22 mutiplied by 13
+
+// Print the result of 22 divided by 13 (as a decimal fraction)
+
+// Print the reminder of 22 divided by 13
+
+function operations() {
+    var num1 = 22;
+    var num2 = 13;
+    console.log(num1 + num2);
+    console.log(num2 - num1);
+    console.log(num2 * num1);
+    console.log(num2 / num1);
+    console.log(num1 % num2);
+}
+
+operations();
