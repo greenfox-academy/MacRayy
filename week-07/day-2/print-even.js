0b465eb247db191de3d4f12f4e89f877b87c7c83
Zsolt Makrai <mecrai@gmail.com>
2017-05-02 16:22:45 +0200

    feat(ex_24): prints even numbers

---
+++ week-07/day-2/print-even.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+
+function printsEvenNumbers () {
+    for (var i = 2; i < 500; i += 2) {
+        console.log(i)
+    }
+}
+
+printsEvenNumbers()
