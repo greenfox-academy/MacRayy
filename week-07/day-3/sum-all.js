ec370e4b5b93831758d5a30e89e25b464abf283f
Zsolt Makrai <mecrai@gmail.com>
2017-05-03 14:40:35 +0200

    feat(sum-all): use reduce method

---
+++ week-07/day-3/sum-all.js
@@ -0,0 +1,11 @@
+'use strict';
+// - Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// - Log the sum of the elements in `ai` to the console
+
+var ai = [3, 4, 5, 6, 7];
+
+ai = ai.reduce(function(a, b){
+    return a + b
+});
+
+console.log(ai);
