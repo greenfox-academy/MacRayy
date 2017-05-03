3b54cffc68202f350456ef13fdd85f8f1ce3bb60
Zsolt Makrai <mecrai@gmail.com>
2017-05-03 15:29:10 +0200

    feat(reverse string): with bulitin methods

---
+++ week-07/day-3/reverse-string.js
@@ -0,0 +1,8 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+reversed = reversed.split('').reverse().join('');
+
+console.log(reversed);
