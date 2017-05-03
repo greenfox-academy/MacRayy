8963062da254f0128cc9f4a290ce408e114e17ec
Zsolt Makrai <mecrai@gmail.com>
2017-05-03 14:50:00 +0200

    feat(reverse): works with bultin method and forEach loop

---
+++ week-07/day-3/reverse.js
@@ -0,0 +1,20 @@
+'use strict';
+// - Create a variable named `aj`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Reverse the order of the elements in `aj`
+// 		- do it with the built in method
+//		- do it with creating a new temp array and a loop
+// - Print the elements of the reversed `aj`
+
+var aj = [3, 4, 5, 6, 7];
+
+console.log(aj.reverse());
+
+var ja = [];
+aj.forEach(function(number) {
+    ja.unshift(number);
+})
+
+aj = ja;
+
+console.log(aj);
