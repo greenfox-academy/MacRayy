b8a2679cd61f9bee4c0d1edcdf12b4c5d0f3fa32
Zsolt Makrai <mecrai@gmail.com>
2017-05-03 11:51:08 +0200

    feat(factorio): working function

---
+++ week-07/day-3/factorio.js
@@ -0,0 +1,13 @@
+'use strict';
+// - Create a function called `factorio`
+//   that returns it's input's factorial
+
+function getFactorio(number) {
+    var factorio = 1;
+    for (var num = 1; num < number + 1; num++) {
+        factorio *= num;
+    };
+    return factorio;
+}
+
+console.log(getFactorio(5));
