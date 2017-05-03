beb6c6e2046814a98d2db8c6892a1dabb14823ae
Zsolt Makrai <mecrai@gmail.com>
2017-05-03 11:23:47 +0200

    feat(double): working function

---
+++ week-07/day-3/doubling.js
@@ -0,0 +1,10 @@
+'use strict';
+// - Create a variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Log the result of `doubling(ak)` to the console
+
+function getDouble(number) {
+    return number * 2;
+}
+
+console.log(getDouble(123));
