07baccb48cb5cd2ea44f78a2a9c034e0c6919624
Zsolt Makrai <mecrai@gmail.com>
2017-05-03 18:49:09 +0200

    feat(hashmap): finally works fine

---
+++ week-07/day-3/hewillnever.js
@@ -0,0 +1,27 @@
+'use strict';
+// Things are a little bit messed up
+// Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
+// Assemble the fragments into the out variable
+
+var notSoCrypticMessage = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
+
+var hashmap = {
+    7: "run around and desert you ",
+    50: "tell a lie and hurt you ",
+    49: "make you cry, ",
+    2: "let you down",
+    12: "give you up, ",
+    1: "Never gonna ",
+    11: "\n",
+    3: "say goodbye "
+};
+
+function decryptMessage(message, lines) {
+    var out = "";
+    message.forEach(function(codeNum) {
+        out += lines[codeNum];
+    });
+    return out;
+}
+
+console.log(decryptMessage(notSoCrypticMessage, hashmap));
