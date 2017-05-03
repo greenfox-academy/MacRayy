9f82e7444b5f3db0e653d5285eacf80c8a2fa46b
Zsolt Makrai <mecrai@gmail.com>
2017-05-03 15:11:12 +0200

    feat(sw): replace method

---
+++ week-07/day-3/simplereplace.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var example = "In a dishwasher far far away";
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+
+var sw = example.replace('dishwasher', 'galaxy');
+example = sw;
+
+console.log(example);
