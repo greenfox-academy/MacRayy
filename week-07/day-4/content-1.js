95fafb35280d25770c0adeb1f5aac1fc8ae2d92c
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 11:46:06 +0200

    feat(content 1): works ok

--- week-07/day-4/content-1.js
+++ week-07/day-4/content-1.js
@@ -5,3 +5,16 @@
 // 3. Alert the content of the second paragraph.
 // 4. Replace the heading's content with 'New content'.
 // 5. Replace the last paragraph's content with the first paragraph's content.
+
+var h1List = document.getElementsByTagName('h1');
+alert(h1List[0].innerText);
+
+var firstParagraph = document.querySelector('p');
+console.log(firstParagraph.innerText);
+
+var paragraphs = document.querySelectorAll('p');
+alert(paragraphs[1].innerText);
+
+h1List[0].innerHTML = 'New content';
+
+paragraphs[2].innerHTML = paragraphs[0].innerText;
