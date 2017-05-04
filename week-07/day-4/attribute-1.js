f1213fcc51903639e7b9550e8e89b83214624813
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 15:03:02 +0200

    feat(attribute 1): html and js created

---
+++ week-07/day-4/attribute-1.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// Write the image's url to the console.
+// Replace the image with a picture of yourself.
+// Make the link point to the Green Fox Academy website.
+// Disable the second button.
+// Replace its text with 'Don't click me!'.
