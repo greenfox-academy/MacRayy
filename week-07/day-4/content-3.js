6a43be24da8abac068e428b8ace8354fec9de609
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 13:46:43 +0200

    feat(content 3): html and js created

---
+++ week-07/day-4/content-3.js
@@ -0,0 +1,7 @@
+'use strict'
+// fill output1 with the first paragraph's content, text only.
+// fill output2 with the first paragraph's content keeping the cat strong.
+
+var firstParagraph = document.querySelector('p');
+var secondParagraph = document.querySelector('.output1');
+var thirdParagraph = document.querySelector('.output2');
