c629a8da1378f35fd29a2f7593f73bb9075ffcbf
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 16:33:13 +0200

    feat(classlist 1): works but yeah...

--- week-07/day-4/classlist-1.js
+++ week-07/day-4/classlist-1.js
@@ -6,3 +6,20 @@
 // If the first p has an 'apple' class, replace cat's content with 'dog'
 // Make apple red
 // Make balloon less balloon-like
+
+var paragraphs = document.querySelectorAll('p');
+
+alert(paragraphs[2].classList.contains('cat'));
+
+if (paragraphs[3].classList.contains('dolphin')) {
+    paragraphs[0].innerHTML = 'pear';
+}
+
+if (paragraphs[0].classList.contains('apple')) {
+    paragraphs[0].innerHTML = 'dog';
+}
+
+paragraphs[0].classList.add('red');
+
+paragraphs[1].style.backgroundColor = 'blue';
+paragraphs[1].style.borderRadius = '5px';
