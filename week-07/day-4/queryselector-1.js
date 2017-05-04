6198a6b77b02d7353d349ca799f3a90a12b39c22
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 12:27:49 +0200

    feat(queryselector 1): works just fine

--- week-07/day-4/queryselector-1.js
+++ week-07/day-4/queryselector-1.js
@@ -16,3 +16,33 @@
 // console.log each of them.
 // 6. store 'The Businessman' in the 'allBizniss' variable.
 // show the result in an 'alert' window.
+
+var king = document.querySelector('#b325').innerText;
+console.log(king);
+
+var conceited = document.querySelectorAll('.asteroid')[1].innerText;
+alert(conceited);
+
+// var asteroidList = document.querySelectorAll('.asteroid');
+// var businessLamp = [];
+// businessLamp.push(asteroidList[2].innerText);
+// businessLamp.push(asteroidList[3].innerText);
+// console.log(businessLamp);
+
+var businessLamp = document.querySelectorAll('.big');
+businessLamp.forEach(function(element) {
+    console.log(element.innerText);
+});
+
+var conceitedKing = document.querySelectorAll('section .asteroid');
+conceitedKing.forEach(function(element) {
+    alert(element.innerText);
+});
+
+var noBusiness = document.querySelectorAll('div');
+noBusiness.forEach(function(element) {
+    console.log(element.innerText);
+});
+
+var allBizniss = document.querySelector('.big');
+alert(allBizniss.innerText);
