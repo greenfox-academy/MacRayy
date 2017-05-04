fc4986355da519dd2ec1fb744533d3b2523b1d94
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 14:58:18 +0200

    feat(content 4): changed the list items

--- week-07/day-4/content-4.js
+++ week-07/day-4/content-4.js
@@ -1,3 +1,9 @@
 'use strict'
 // replace the list items' content with items from this list:
-// ['apple', 'banana', 'cat', 'dog']
+
+var list = ['apple', 'banana', 'cat', 'dog'];
+var listItems = document.querySelectorAll('li');
+
+for (var el = 0; el < listItems.length; el++) {
+    listItems[el].innerText = list[el];
+}
