a39e2268d23591a976a762c5b3a55c6864d1875e
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 17:37:58 +0200

    feat(create element 2): functions 👏🏾

--- week-07/day-4/createelement-2.js
+++ week-07/day-4/createelement-2.js
@@ -1,3 +1,26 @@
 'use strict'
 // Remove the king from the list.
 // Add 3 list items saying 'The Fox' to the list.
+
+var asteroidsList = document.querySelector('.asteroids');
+var firstLi = document.querySelector('li');
+asteroidsList.removeChild(firstLi);
+
+function createListItems() {
+    var newListItems = []
+    for (var i = 0; i < 3; i++) {
+        var newListItem = document.createElement('li');
+        newListItem.innerText = 'The Fox ' + (i + 1);
+        newListItems.push(newListItem);
+    }
+    return newListItems;
+}
+
+function addListItems(listItems) {
+    for (var i = 0; i < 3; i++) {
+        asteroidsList.appendChild(listItems[i]);
+    }
+}
+
+// createListItems();
+addListItems(createListItems());
