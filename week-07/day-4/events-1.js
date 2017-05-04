812e194a0400ede6ce5a6a19c27240f788263360
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 18:18:49 +0200

    feat(events 1): party on and off 🎊

--- week-07/day-4/events-1.js
+++ week-07/day-4/events-1.js
@@ -1,2 +1,15 @@
 'use strict'
 // Turn the party on and off by clicking the button.
+
+var body = document.querySelector('body');
+var button = document.querySelector('button');
+
+function partyOnOff() {
+    if (body.classList.contains('party')) {
+        body.classList.remove('party');
+    } else {
+        body.classList.add('party');
+    }
+}
+
+button.addEventListener('click', partyOnOff);
