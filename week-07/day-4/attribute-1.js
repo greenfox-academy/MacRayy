1a5106b18514e5635ee5f71cbcf2b356649520ab
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 15:46:42 +0200

    feat(attribute 1): image changed and link points to greenfox

--- week-07/day-4/attribute-1.js
+++ week-07/day-4/attribute-1.js
@@ -5,3 +5,17 @@
 // Make the link point to the Green Fox Academy website.
 // Disable the second button.
 // Replace its text with 'Don't click me!'.
+
+var img = document.querySelector('img');
+
+console.log(img.getAttribute('src'));
+
+img.setAttribute('src', '/Users/MrFox/OneDrive/greenfox/MacRayy/week-07/day-4/hero-down.jpg');
+
+var link = document.querySelector('a');
+
+link.setAttribute('href', 'https://www.greenfoxacademy.com/');
+
+var button = document.querySelector('.this-one');
+
+button.innerText = 'Don\'t click me';
