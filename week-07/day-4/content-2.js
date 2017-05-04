ca968995f66429d37d13a066f2db916c7188a6fe
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 13:37:41 +0200

    feat(content 2): last p's contentin every ps

--- week-07/day-4/content-2.js
+++ week-07/day-4/content-2.js
@@ -1,3 +1,10 @@
 'use strict'
 
 // fill every paragraph with the last one's content.
+
+var paragraphList = document.querySelectorAll('p');
+var lastParagraph = document.querySelector('.dog');
+
+for (var p = 0; p < paragraphList.length - 1; p++) {
+    paragraphList[p].innerText = lastParagraph.innerText;
+}
