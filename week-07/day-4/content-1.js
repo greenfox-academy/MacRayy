4de3fb57851bf7c8c5b44146b326d94c7aef6839
Zsolt Makrai <mecrai@gmail.com>
2017-05-04 10:54:53 +0200

    feat(content 1): html and js created

---
+++ week-07/day-4/content-1.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// 1. Alert the content of the heading.
+// 2. Write the content of the first paragraph to the console.
+// 3. Alert the content of the second paragraph.
+// 4. Replace the heading's content with 'New content'.
+// 5. Replace the last paragraph's content with the first paragraph's content.
