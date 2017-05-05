2cbe45c12b5805f00f09cfb89528cfecf06e418b
Zsolt Makrai <mecrai@gmail.com>
2017-05-05 14:03:13 +0200

    feat(galery): goes around in both ways 👊🏿

--- week-07/day-5/gallery.js
+++ week-07/day-5/gallery.js
@@ -36,10 +36,11 @@ function changeImgSrcRight() {
     console.log(listIndex);
     for (var i = 0; i < imageList.length; i++) {
         if (i === listIndex) {
-            mainImage.setAttribute('src', imageList[i].imgSrc);
-        } else if (listIndex === -1) {
-            listIndex = imageList.length;
+            mainImage.setAttribute('src', imageList[listIndex].imgSrc);
+        }  else if (listIndex === -1) {
+            listIndex = imageList.length - 1;
             mainImage.setAttribute('src', imageList[imageList.length - 1].imgSrc);
+            console.log(listIndex);
         }
     }
 }
