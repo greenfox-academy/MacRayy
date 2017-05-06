559ad7af63c7842ef82a330f0ae5d0ebdc65106d
Zsolt Makrai <mecrai@gmail.com>
2017-05-06 22:17:30 +0200

    feat(bookmarklet 1): works only one page

--- week-07/day-4/bookmarklet-1.js
+++ week-07/day-4/bookmarklet-1.js
@@ -2,3 +2,14 @@
 // Create a script that replaces every h1 headline's content
 // with 'Green Fox Academy Conquers the World'.
 // Create a bookmarklet that does that on any website.
+
+var link = document.querySelector('a');
+var titleList = document.querySelectorAll('h1');
+
+function ruleTheWorld() {
+    for (var i = 0; i < titleList.length; i++) {
+        titleList[i].innerHTML = 'Green Fox Academy Conquers the World';
+    }
+}
+
+// link.setAttribute('href', ruleTheWorld());
