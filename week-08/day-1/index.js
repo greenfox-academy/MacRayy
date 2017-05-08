9c7a73b9ebc17249950270c988d9009434709240
Zsolt Makrai <mecrai@gmail.com>
2017-05-08 12:03:52 +0200

    feat(sodoku validator): failing tests for unique lists

--- week-08/day-1/index.js
+++ week-08/day-1/index.js
@@ -9,9 +9,11 @@ const test = require('tape');
 // [9,8,7,6,5,1,2,4,9] => Should fail
 // [9,8,7,6,5,1,2,4,'u'] => Should fail (non integer)
 
-var sodokuList = [];
-var sodokuList1 = [9,0,0,4,5,6,7,8,9];
-var sodokuList2 = [9,0,0,4,5,6,7,8,'n'];
+var sodokuList1 = [];
+var sodokuList2 = [9,0,0,4,5,6,7,8];
+var sodokuList3 = [9,0,0,4,5,6,7,8,'n'];
+var sodokuList4 = [9,0,0,4,5,6,7,8,9];
+var sodokuList5 = [1,0,0,4,5,6,7,8,9];
 
 function validator(list) {
     if (list.length === 9) {
@@ -21,7 +23,6 @@ function validator(list) {
         if (list.every(isItNumber) === false ) {
             throw new Error('Pls add only numbers')
         }
-        return true
     } else {
         return false;
     }
@@ -29,18 +30,24 @@ function validator(list) {
 
 // TEST
 test('test validator is undefined', function(t) {
-    t.equal(validator(sodokuList), false);
+    t.equal(validator(sodokuList1), false);
     t.end();
 })
 
 test('test length', function(t){
-    t.equal(validator(sodokuList1), true);
+    t.equal(validator(sodokuList2), false);
     t.end();
 })
 
 test ('is it number?', function(t){
     t.throws(function(){
-        validator(sodokuList2);
+        validator(sodokuList3);
     }, Error);
+    t.end();
+})
+
+test ('only one kind of number', function(t) {
+    t.equal(validator(sodokuList4), false);
+    t.equal(validator(sodokuList5), true);
     t.end();
 })
