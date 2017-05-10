cb2efd27fef8d5a2adf896aac142c4de8bc9dbbc
Zsolt Makrai <mecrai@gmail.com>
2017-05-10 09:45:18 +0200

    feat(examples): map function

--- week-08/day-3/examples.js
+++ week-08/day-3/examples.js
@@ -1,10 +1,25 @@
 'use strict'
 
 const numbers = [1,2,3,4,5];
+const numbersWithString = ['alma', 1,2,3,4,5];
 
-let = doubleNumbers = numbers.map(function(num) {
+let doubleNumbers = numbers.map(function(num) {
     return num *2;
 });
 
 console.log(doubleNumbers);
 // a map csinal egy uj listat es minden elemet beletolt egy uj listaba a megadott feltetelek alapjan
+
+let bigger = numbers.filter(function(num) {
+    return num > 2;
+})
+
+console.log(bigger);
+
+let onlyNumbers = numbersWithString.filter(function(num) {
+    return num * 2;
+}) // megnezi hogy mi szorozhato kettovel.
+
+console.log(onlyNumbers);
+// filter csinal egy uj listat a megadott feltetel alapjan.
+// kb mint egy for ciklus amiben van egy if statement.
