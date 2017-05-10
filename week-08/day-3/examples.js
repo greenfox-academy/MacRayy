15f564ae3117b40407f86013f2a839b5b01780c8
Zsolt Makrai <mecrai@gmail.com>
2017-05-10 09:38:37 +0200

    feat(examples): map function

---
+++ week-08/day-3/examples.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const numbers = [1,2,3,4,5];
+
+let = doubleNumbers = numbers.map(function(num) {
+    return num *2;
+});
+
+console.log(doubleNumbers);
+// a map csinal egy uj listat es minden elemet beletolt egy uj listaba a megadott feltetelek alapjan
