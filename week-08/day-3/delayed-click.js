7c690a257874c2b51fbec16b6c0fa3440490e607
Zsolt Makrai <mecrai@gmail.com>
2017-05-10 15:02:18 +0200

    feat(delayed click): if button pushed writes a message

--- week-08/day-3/delayed-click.js
+++ week-08/day-3/delayed-click.js
@@ -1,2 +1,18 @@
 'use strict'
 // Create a simple HTML document with one button. If the user clicks the button it should wait 2 seconds and it should show a text under the button: 2 seconds ellapsed
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+
+let timeCounter = function() {
+    setTimeout(function() {
+        writeMessage();
+    }, 2000);
+}
+
+let writeMessage = function() {
+    let message = document.createElement('p');
+    message.innerText = '2 seconds ellapsed'
+    body.appendChild(message);
+}
+
+button.addEventListener('click', timeCounter);
