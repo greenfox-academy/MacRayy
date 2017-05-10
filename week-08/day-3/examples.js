b476b0017f36c406589135ef8a08e91997aa2c2b
Zsolt Makrai <mecrai@gmail.com>
2017-05-10 09:52:19 +0200

    feat(examples): asynchronicity

--- week-08/day-3/examples.js
+++ week-08/day-3/examples.js
@@ -23,3 +23,20 @@ let onlyNumbers = numbersWithString.filter(function(num) {
 console.log(onlyNumbers);
 // filter csinal egy uj listat a megadott feltetel alapjan.
 // kb mint egy for ciklus amiben van egy if statement.
+
+function greet1() {
+    console.log('hallo 1');
+}
+
+function greet2() {
+    console.log('szia uramr');
+}
+
+function greet3() {
+    console.log('hallo 2');
+}
+
+// aszinkronitas
+greet1();
+setTimeout(greet2, 1000);
+greet3();
