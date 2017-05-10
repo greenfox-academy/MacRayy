6bfd8421724e5e7670c8c4315060a58636d355bf
Zsolt Makrai <mecrai@gmail.com>
2017-05-10 16:12:06 +0200

    feat(three times click): write message after time is over

--- week-08/day-3/click-three-times.js
+++ week-08/day-3/click-three-times.js
@@ -1,31 +1,34 @@
 'use strict'
 // Create a simple HTML document with one button. If the user clicks the button 3 times, and 5 seconds is already ellapsed since the page is loaded, a text should apper under the button: 5 seconds ellapsed and clicked 3 times
 let timeOver = false;
-let clicks = 0
+let clicks = 0;
 const body = document.querySelector('body');
 const button = document.querySelector('button');
-
-
-let timeCounter = function() {
-    setTimeout(function() {
-        timeOver = true;
-        if (clicks >= 3) {
-            writeMessage();
-        }
-    }, 2000);
-}
-
-let clickCounter = function() {
-    clicks++;
-    console.log(clicks);
-}
 
 let writeMessage = function() {
     let message = document.createElement('p');
     message.innerText = '5 seconds ellapsed and clicked 3 times'
     body.appendChild(message);
+};
+
+let timeCounter = function() {
+    setTimeout(function() {
+        timeOver = true;
+        checkState();
+    }, 5000);
+};
+
+let checkState = function() {
+    if (clicks >= 3 && timeOver) {
+        writeMessage();
+    }
+};
+
+let addClicks = function() {
+    clicks++;
+    checkState();
 }
 
 timeCounter();
 
-button.addEventListener('click', clickCounter)
+button.addEventListener('click', addClicks);
