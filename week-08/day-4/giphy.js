40d7083570a7b7ec676d9d5684d8160060459ea5
Zsolt Makrai <mecrai@gmail.com>
2017-05-11 13:28:29 +0200

    feat(gyphy): display the images

--- week-08/day-4/giphy.js
+++ week-08/day-4/giphy.js
@@ -7,31 +7,33 @@
 const body = document.querySelector('body');
 
 let controller = function() {
-    createList()
     let request = new XMLHttpRequest();
     request.open('GET', 'http://api.giphy.com/v1/gifs/search?q=buster+keaton&api_key=dc6zaTOxFJmzC', true);
     request.send();
 
     request.onreadystatechange = function() {
         if (request.readyState === 4 && request.status === 200) {
-            var gif = JSON.parse(request.response);
-            console.log(gif);
+            var gifList = JSON.parse(request.response);
+            console.log(gifList);
+            var imageList = [];
+            for (let i = 0; i < 16; i++) {
+                var image = gifList.data[i].images.downsized_still.url;
+                imageList.push(image);
+            }
+            createList(gifList)
         }
     }
 
+    let createList = function(gifList) {
+        let unorderdList = document.createElement('ul');
+        body.appendChild(unorderdList);
+        for (let i = 0; i < 16; i++) {
+            let listItem = document.createElement('li');
+            listItem.style.backgroundImage = 'url(' + gifList.data[i].images.downsized_still.url + ')';
+            unorderdList.appendChild(listItem)
+        }
+    }
 }
 
-let createList = function() {
-    let unorderdList = document.createElement('ul');
-    body.appendChild(unorderdList);
-    let listItemsArray = [];
-    for (var i = 0; i < 16; i++) {
-        let listItem = document.createElement('li');
-        listItemsArray.push(listItem);
-    }
-    for (var i = 0; i < 16; i++) {
-        unorderdList.appendChild(listItemsArray[i])
-    }
-}
 
 const gifs = new controller;
