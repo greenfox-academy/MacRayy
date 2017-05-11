351893ec218232bb7c92409fef3818d78df36f44
Zsolt Makrai <mecrai@gmail.com>
2017-05-11 16:30:22 +0200

    feat(nyt): get data function works

--- week-08/day-4/new-york-times.js
+++ week-08/day-4/new-york-times.js
@@ -10,3 +10,29 @@
 // Snippet
 // Publication date
 // Create a permalink to that article
+
+let ajax = function (url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let rsp = JSON.parse(xhr.response);
+                callback(rsp);
+                console.log(rsp);
+            }
+        } else {
+            console.log(xhr.status);
+        }
+    }
+    xhr.send()
+}
+
+let getNYTData = function() {
+    const endpont = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=star+wars&api_key=2cccf4def763418d933793e8a1f01630';
+    ajax(endpont, function(response) {
+        // displayNYTData(response)
+    });
+}
+
+getNYTData();
