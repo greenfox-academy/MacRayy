89b0c3cdde357ab7e79fbca08bc4ec050745125a
Zsolt Makrai <mecrai@gmail.com>
2017-05-11 15:35:23 +0200

    feat(nyt): html and js created

---
+++ week-08/day-4/new-york-times.js
@@ -0,0 +1,12 @@
+'use strict'
+// Search interesting articles on: http://developer.nytimes.com/
+//
+// Ask your local mentor for the API key or request your own. Use localhost as the website.
+//
+// Use the Article Search API
+// Find articles about the moon landing by Apollo 11
+// Display the following fields in a list
+// Headline
+// Snippet
+// Publication date
+// Create a permalink to that article
