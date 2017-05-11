20aa67d2c3460971d8e24146545a6d1239b7f630
Zsolt Makrai <mecrai@gmail.com>
2017-05-11 10:20:09 +0200

    feat(gyphy): html and js created

---
+++ week-08/day-4/giphy.js
@@ -0,0 +1,6 @@
+'use strict'
+// Display gifs of a cute/funny topic using: https://github.com/Giphy/GiphyAPI
+//
+// Search/Find the images in the API
+// Display the list of the first 16 results's static thumbnail
+// If the user clicks on the thumbnail, display the animated GIF
