1aa22b2f05451e106ed856a0cb63be8dc5c0d74e
Zsolt Makrai <mecrai@gmail.com>
2017-05-15 09:10:54 +0200

    html and css created

---
+++ week-08/day-4/get-position.js
@@ -0,0 +1,12 @@
+'use strict'
+// Get your input city's position using this API: https://market.mashape.com/dev132/city-geo-location-lookup
+//
+// Ask your local mentor for the API key or request your own. Use localhost as the website.
+//
+// Create a very simple HTML:
+// it should have a label
+// an input field (text) for the name of city
+// and a button
+// The request should be sent to the API by clicking the button
+// If you get the response, you should display your city's coordinates: latitude, longitude
+// Mark the city on a map, using Google Maps API
