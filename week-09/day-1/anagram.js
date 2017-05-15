725362132ec650b39775cf78b3f1a861822db085
Zsolt Makrai <mecrai@gmail.com>
2017-05-15 16:18:18 +0200

    feat(anagram): compare letters working, tested

--- week-09/day-1/anagram.js
+++ week-09/day-1/anagram.js
@@ -8,6 +8,21 @@ let areAnagramms = function(word1, word2) {
 
     this.anagrammLength = function() {
         if (this.word1.length === this.word2.length) {
+            this.anagrammLetters();
+        } else {
+            return false;
+        }
+    }
+
+    this.anagrammLetters = function() {
+        let wordList1 = [];
+        let wordList2 = [];
+        wordList1 = this.word1.split('').sort();
+        wordList2 = this.word2.split('').sort();
+        let sortedword1 = wordList1.join();
+        let sortedeord2 = wordList2.join();
+
+        if (sortedword1 === sortedeord2) {
             return true;
         } else {
             return false;
--- week-09/day-1/anagram_test.js
+++ week-09/day-1/anagram_test.js
@@ -2,10 +2,16 @@
 const test = require('tape');
 const areAnagramms = require('./anagram.js');
 
-test('sameLength', function(t) {
-    let anagramm1 = new areAnagramms('dog', 'cat');
-    t.equal(anagramm1.anagrammLength(), true);
+test('same Length', function(t) {
     let anagramm2 = new areAnagramms('dog', 'gold');
     t.equal(anagramm2.anagrammLength(), false);
+    t.end();
+});
+
+test('same Letters', function(t) {
+    let anagramm1 = new areAnagramms('dog', 'cat');
+    t.equal(anagramm1.anagrammLetters(), false)
+    let anagramm2 = new areAnagramms('dog', 'god');
+    t.equal(anagramm2.anagrammLetters(), true)
     t.end();
 });
