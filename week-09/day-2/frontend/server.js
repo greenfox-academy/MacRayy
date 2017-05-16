112a5c3832eaf6945647a50b26023e2360c867a9
Zsolt Makrai <mecrai@gmail.com>
2017-05-16 15:47:34 +0200

    feat(greeter): greeter function sends message

--- week-09/day-2/frontend/server.js
+++ week-09/day-2/frontend/server.js
@@ -6,25 +6,39 @@ const app = express();
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
-})
+});
 
 app.use('/assets', express.static('assets'));
 
 app.get('/doubling', function(req, res) {
-    console.log(req.query);
-    console.log(req.query.input);
     if (req.query === {} || req.query.input === undefined) {
         res.send( {
-            error: "Please provide an input!"
-        })
+            error: 'Please provide an input!'
+        });
     } else {
         res.send( {
             received: parseInt(req.query.input),
             result: req.query.input * 2
-        })
+        });
     }
-})
+});
+
+app.get('/greeter', function(req, res) {
+    if (req.query.name === undefined) {
+        res.send( {
+            error: 'Please provide a name!'
+        });
+    } else if (req.query.title === undefined) {
+        res.send( {
+            error: 'Please provide a title!'
+        });
+    } else {
+        res.send( {
+            welcome_message: 'Oh, hi there ' + req.query.name + ', my dear ' + req.query.title +'!'
+        });
+    }
+});
 
 app.listen(8080, function functionName() {
     console.log('server is running');
-})
+});
