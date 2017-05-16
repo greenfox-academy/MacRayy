78fbbc2d1ee79602ef46b01461d6f4e84557902c
Zsolt Makrai <mecrai@gmail.com>
2017-05-16 14:01:06 +0200

    feat(doubling): doubles the input and handles the error

--- week-09/day-2/frontend/server.js
+++ week-09/day-2/frontend/server.js
@@ -10,9 +10,20 @@ app.get('/', function(req, res) {
 
 app.use('/assets', express.static('assets'));
 
-// app.get('/doubling', function(req, res) {
-//     res.
-// })
+app.get('/doubling', function(req, res) {
+    console.log(req.query);
+    console.log(req.query.input);
+    if (req.query === {} || req.query.input === undefined) {
+        res.send( {
+            error: "Please provide an input!"
+        })
+    } else {
+        res.send( {
+            received: parseInt(req.query.input),
+            result: req.query.input * 2
+        })
+    }
+})
 
 app.listen(8080, function functionName() {
     console.log('server is running');
