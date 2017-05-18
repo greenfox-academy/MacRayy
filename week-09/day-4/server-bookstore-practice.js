e869002a365f1b5268a001c319b87a25285322d7
Zsolt Makrai <mecrai@gmail.com>
2017-05-18 15:12:42 +0200

    feat(bookstore): listing works with string

--- week-09/day-4/server-bookstore-practice.js
+++ week-09/day-4/server-bookstore-practice.js
@@ -27,12 +27,45 @@ app.get('/', (req, res) => {
             console.log('Error: ', err);
         } else {
             rows.forEach(row => {
-                HTML += '<li>' + (row.book_name) + '</li>';
+                HTML += '<li>' + row.book_name + '</li>';
             });
         }
         HTML += '</ul>'
         res.send(HTML);
     });
+});
+
+
+app.get('/table', (req, res) => {
+    const tableData = 'book_name, aut_name, cate_descrip, pub_name, book_price';
+    const bookTable = 'book_mast';
+    const authorTable = 'author';
+    const categoryTable = 'category';
+    const publisherTable = 'publisher';
+    const authorID = 'book_mast.aut_id = author.aut_id';
+    const categoryID = 'book_mast.cate_id = category.cate_id';
+    const publisherID = 'book_mast.pub_id = publisher.pub_id';
+    const sql = 'SELECT ? FROM ? INNER JOIN ? ON ? INNER JOIN ? ON ? INNER JOIN ? ON ?'
+    const sqlData = [tableData, bookTable, authorTable, authorID, categoryTable, categoryID, publisherTable, publisherID]
+    let HTML = '<table>';
+    // conn.query('SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast INNER JOIN author ON book_mast.aut_id = author.aut_id INNER JOIN category ON book_mast.cate_id = category.cate_id INNER JOIN publisher ON book_mast.pub_id = publisher.pub_id', (err, rows) => {
+    conn.query(sql, sqlData, (err, rows) => {
+        console.log('table');
+        if (err) {
+            console.log('Error: ', err);
+        } else {
+            rows.forEach(row => {
+                HTML += '<tr><td>' + row.book_name + '</td>';
+                HTML += '<td>' + row.aut_name + '</td>'
+                HTML += '<td>' + row.cate_descrip + '</td>'
+                HTML += '<td>' + row.pub_name + '</td>'
+                HTML += '<td>' + row.book_price + '</td></tr>';
+            });
+        }
+        HTML += '</table>'
+        res.send(HTML)
+    })
+
 });
 
 app.listen(3000, () => {
