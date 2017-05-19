368059306e5dce664b413facad732cc569d266c3
Zsolt Makrai <mecrai@gmail.com>
2017-05-19 10:47:43 +0200

    feat(reddit server): sql DB created, server displays the data from the DB

--- week-09/day-5/server.js
+++ week-09/day-5/server.js
@@ -6,11 +6,38 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'Reddit'
+});
+
+conn.connect(err => {
+  if(err){
+    console.log("Error connecting to Db");
+    return;
+  }
+  console.log("Connection established\n");
+});
+
 app.get('/hello', (req, res) => {
     const hello = function() {
         return 'hello world';
     }
     res.send(hello());
+});
+
+app.get('/post', (req, res) => {
+    let result = {"post": 0};
+    conn.query('SELECT * FROM posts', (err, rows) => {
+        if (err) {
+            console.log('Error: ', err);
+        } else {
+            result = rows;
+        }
+        res.send(result);
+    });
 });
 
 const port = 3000;
