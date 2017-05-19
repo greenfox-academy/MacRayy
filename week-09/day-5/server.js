501315432fa32877889cb4807663098f0f33c84d
Zsolt Makrai <mecrai@gmail.com>
2017-05-19 09:54:44 +0200

    feat(reddit server): server is running

--- week-09/day-5/server.js
+++ week-09/day-5/server.js
@@ -3,3 +3,17 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+    const hello = function() {
+        return 'hello world';
+    }
+    res.send(hello());
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('server running on ' + port);
+})
