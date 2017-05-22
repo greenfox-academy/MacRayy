13b08edcececb809d3e8be487ca093d56ca4aeca
Zsolt Makrai <mecrai@gmail.com>
2017-05-22 12:38:17 +0200

    refactor(tenis): tenisgame1 get score function modified

--- week-10/day-1/tennisgame1.js
+++ week-10/day-1/tennisgame1.js
@@ -17,58 +17,76 @@ TennisGame1.prototype.wonPoint = function(playerName) {
 
 TennisGame1.prototype.getScore = function() {
     let score = "";
-    let tempScore = 0;
     if (this.m_score1 === this.m_score2) {
-        switch (this.m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
+        score = this.getEqualScore();
     } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-        let minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) {
-            score = "Advantage player1";
-        } else if (minusResult === -1) {
-            score = "Advantage player2";
-        } else if (minusResult >= 2) {
-            score = "Win for player1";
-        } else {
-            score = "Win for player2";
-        }
+        score = this.getScoreOverFour ();
     } else {
-        tempScore = this.m_score1;
-        for (let i = 1; i < 3; i++) {
-            if (i > 1) {
-                score += "-";
-                tempScore = this.m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
-        }
+        score = this.getScoreBelowThree ();
     }
     return score;
 };
+
+TennisGame1.prototype.getEqualScore = function() {
+    let scoreEqual = '';
+    switch (this.m_score1) {
+        case 0:
+            scoreEqual = "Love-All";
+            break;
+        case 1:
+            scoreEqual = "Fifteen-All";
+            break;
+        case 2:
+            scoreEqual = "Thirty-All";
+            break;
+        default:
+            scoreEqual = "Deuce";
+            break;
+    }
+    return scoreEqual;
+}
+
+TennisGame1.prototype.getScoreOverFour  = function() {
+    let scoreOverFour = '';
+    let minusResult = this.m_score1 - this.m_score2;
+    if (minusResult === 1) {
+        scoreOverFour = "Advantage player1";
+    } else if (minusResult === -1) {
+        scoreOverFour = "Advantage player2";
+    } else if (minusResult >= 2) {
+        scoreOverFour = "Win for player1";
+    } else {
+        scoreOverFour = "Win for player2";
+    }
+    return scoreOverFour;
+}
+
+TennisGame1.prototype.getScoreBelowThree = function() {
+    let scoreBelowThree = '';
+    for (let i = 1; i < 3; i++) {
+        if (i === 1) {
+            scoreBelowThree = this.m_score1;
+        } else {
+            scoreBelowThree += '-'
+            switch (this.m_score2) {
+                case 0:
+                scoreBelowThree += "Love";
+                break;
+                case 1:
+                scoreBelowThree += "Fifteen";
+                break;
+                case 2:
+                scoreBelowThree += "Thirty";
+                break;
+                case 3:
+                scoreBelowThree += "Forty";
+                break;
+            }
+        }
+    }
+    console.log(scoreBelowThree);
+    return scoreBelowThree
+}
 
 if (typeof window === "undefined") {
     module.exports = TennisGame1;
