cd0c3ca28d4c1a7f855b16b817be5ae8779cd042
Zsolt Makrai <mecrai@gmail.com>
2017-05-22 10:54:12 +0200

    refactor(tenis): tenisgame3 getScore =, () added

--- week-10/day-1/tennisgame3.js
+++ week-10/day-1/tennisgame3.js
@@ -9,16 +9,16 @@ var TennisGame3 = function(p1N, p2N) {
 };
 
 TennisGame3.prototype.getScore = function() {
-    var s;
+    let s;
     if ((this.p1 < 4 && this.p2 < 4) && (this.p1 + this.p2 < 6)) {
-        var p = ["Love", "Fifteen", "Thirty", "Forty"];
+        let p = ["Love", "Fifteen", "Thirty", "Forty"];
         s = p[this.p1];
-        return (this.p1 == this.p2) ? s + "-All" : s + "-" + p[this.p2];
+        return (this.p1 === this.p2) ? s + "-All" : s + "-" + p[this.p2];
     } else {
-        if (this.p1 == this.p2)
+        if (this.p1 === this.p2)
             return "Deuce";
-        s = this.p1 > this.p2 ? this.p1N : this.p2N;
-        return ((this.p1 - this.p2) * (this.p1 - this.p2) == 1) ? "Advantage " + s : "Win for " + s;
+        s = (this.p1 > this.p2) ? this.p1N : this.p2N;
+        return ((this.p1 - this.p2) * (this.p1 - this.p2) === 1) ? "Advantage " + s : "Win for " + s;
     }
 };
 
