e4970c91f7363a507532ec5411ad7fdec83099e2
Zsolt Makrai <mecrai@gmail.com>
2017-05-23 11:44:16 +0200

    feat(animal): prototypes works

--- week-10/day-2/animal.js
+++ week-10/day-2/animal.js
@@ -3,3 +3,19 @@
 // Create a constructor for creating Animals.
 // it should take one parameter: what the animal says
 // Every animal should have a method called say() that prints what the animal says
+
+const Animal = function(sound) {
+    this.sound = sound;
+};
+
+Animal.prototype.say = function() {
+    console.log(this.sound);
+};
+
+const dog = new Animal('woof');
+
+dog.say();
+
+const wolf = new Animal('howl');
+
+wolf.say();
