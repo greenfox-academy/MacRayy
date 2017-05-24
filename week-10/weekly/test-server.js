e615aa654bd6da61ff359952a8d7e05dd43dd4aa
Zsolt Makrai <mecrai@gmail.com>
2017-05-24 14:36:21 +0200

    feat(player): scripts created and added to html, mustache added

--- week-10/weekly/test-server.js
+++ week-10/weekly/test-server.js
@@ -10,7 +10,6 @@ const playlistsData = [
 	{ "id": 3, "title": "Driving", "system": 0},
 	{ "id": 5, "title": "Fox house", "system": 0},
 ];
-
 const tracksData = [
 	{ "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "c:/music/halahula.mp3" },
 	{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" }
