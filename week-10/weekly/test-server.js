7bae6c75fcb901913238ce5015149a22f302b73c
Zsolt Makrai <mecrai@gmail.com>
2017-05-24 13:24:38 +0200

    feat(server): server has a  statis html and sends the json objacts

--- week-10/weekly/test-server.js
+++ week-10/weekly/test-server.js
@@ -4,9 +4,30 @@ const express = require('express');
 
 const app = express();
 
+const playlistsData = [
+	{ "id": 1, "title": "Favorites", "system": 1},
+	{ "id": 2, "title": "Music for programming", "system": 0},
+	{ "id": 3, "title": "Driving", "system": 0},
+	{ "id": 5, "title": "Fox house", "system": 0},
+];
+
+const tracksData = [
+	{ "id": 21, "title": "Halahula", "artist": "Untitled artist", "duration": 545, "path": "c:/music/halahula.mp3" },
+	{ "id": 412, "title": "No sleep till Brooklyn", "artist": "Beastie Boys", "duration": 312.12, "path": "c:/music/beastie boys/No sleep till Brooklyn.mp3" }
+];
+
+app.use('/assets', express.static('assets'));
+
 app.get('/', (req, res) => {
-    console.log('hello');
-    res.send('hello');
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/playlists', (req, res) => {
+    res.send(playlistsData);
+});
+
+app.get('/playlist-tracks', (req, res) => {
+    res.send(tracksData);
 });
 
 app.listen(3000, () => {
