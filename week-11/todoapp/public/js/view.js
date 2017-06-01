880c81475a72a87dad6ecb60e4ec8c38f55acc5e
Zsolt Makrai <mecrai@gmail.com>
2017-06-01 15:21:05 +0200

    feat(display): writes the todos from the DB, but everything is unchecked

--- week-11/todoapp/public/js/view.js
+++ week-11/todoapp/public/js/view.js
@@ -1,9 +1,16 @@
 'use strict'
 
 const createDisplay = function() {
+    const todoList = document.querySelector('#todo-list');
 
-    const renderTodos = function() {
-        console.log('render');
+    const renderTodos = (response) => {
+        const todos = {todos: response};
+        const displayTodos = '{{#todos}} <li class="list-item">{{todo_name}}<p class="icons"><span class="del"></span><span class="unchecked"></span></p></li> {{/todos}}'
+        console.log(displayTodos);
+        console.log(todoList);
+        const compiledTodos = Handlebars.compile(displayTodos);
+        const generatedHTML = compiledTodos(todos);
+        todoList.innerHTML = generatedHTML;
     };
 
     return {
