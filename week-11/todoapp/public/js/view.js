a555c415e0b26abc4256944ad0d1f9303d545953
Zsolt Makrai <mecrai@gmail.com>
2017-06-01 15:56:31 +0200

    refactor(display): remove console.log

--- week-11/todoapp/public/js/view.js
+++ week-11/todoapp/public/js/view.js
@@ -6,7 +6,6 @@ const createDisplay = function() {
     const renderTodos = (response) => {
         const todos = {todos: response};
         const displayTodos = '{{#todos}} {{#if completed}} <li class="list-item">{{todo_name}}<p class="icons"><span class="del"></span><span class="checked"></span></p></li> {{else}} <li class="list-item">{{todo_name}}<p class="icons"><span class="del"></span><span class="unchecked"></span></p></li> {{/if}} {{/todos}}'
-        console.log(todoList);
         const compiledTodos = Handlebars.compile(displayTodos);
         const generatedHTML = compiledTodos(todos);
         todoList.innerHTML = generatedHTML;
