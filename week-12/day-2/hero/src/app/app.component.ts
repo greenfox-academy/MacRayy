346a627aa76c9b8aadb8a5f7b22be9f091f38752
Zsolt Makrai <mecrai@gmail.com>
2017-06-13 15:27:02 +0200

    feat(hero): can select hero and displays it, css added to selected hero

--- week-12/day-2/hero/src/app/app.component.ts
+++ week-12/day-2/hero/src/app/app.component.ts
@@ -8,11 +8,16 @@ import { HEROES } from './heroes';
   styleUrls: ['./app.component.css']
 })
 
+
 export class AppComponent {
     title = 'Tour of Heroes';
     heroes = HEROES;
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
+    // hero: Hero = {
+    //     id: 1,
+    //     name: 'Windstorm'
+    // };
+    selectedHero: Hero;
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
     };
 }
