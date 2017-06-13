26e1ab43fa971c90d43c791d6b40fc0ec697023b
Zsolt Makrai <mecrai@gmail.com>
2017-06-13 23:08:58 +0200

    feat(hero): services

--- week-12/day-2/hero/src/app/app.component.ts
+++ week-12/day-2/hero/src/app/app.component.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 import { Hero } from './hero';
 import { HEROES } from './heroes';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
 
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'Tour of Heroes';
-    heroes = HEROES;
-    // hero: Hero = {
-    //     id: 1,
-    //     name: 'Windstorm'
-    // };
+    heroes: Hero[];
     selectedHero: Hero;
     onSelect(hero: Hero): void {
         this.selectedHero = hero;
     };
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    constructor(private heroService: HeroService) { }
+
 }
