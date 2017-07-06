908b3b1cc183269435b4f8824f3b58d4f19f4084
Zsolt Makrai <mecrai@gmail.com>
2017-07-06 11:11:19 +0200

    feat(react): practice

--- react/FirstMobile/App.js
+++ react/FirstMobile/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, Image, StyleSheet, Text, View, TextInput } from 'react-native';
 
-export default class FlexDimensionsBasics extends Component {
-  render() {
-    return (
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center'
-      }}>
-        <View style={{width: 100, height: 100, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'steelblue'}} />
-      </View>
-    );
-  }
+export default class PizzaTranslator extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {text: ''}
+    }
+
+    render () {
+        return (
+            <View style={{padding: 10}}>
+                <TextInput
+                    style={{height: 40}}
+                    placeholder="Type here to translate!"
+                    onChangeText={(text) => this.setState({text})}
+                />
+                <Text style={{padding: 10, fontSize: 42}}>
+                    {this.state.text.split(' ').map((word) => word && '🍕 and 💩').join(' ')}
+                </Text>
+            </View>
+        );
+    }
 }
+
+// export default class FlexDimensionsBasics extends Component {
+//   render() {
+//     return (
+//       // Try removing the `flex: 1` on the parent View.
+//       // The parent will not have dimensions, so the children can't expand.
+//       // What if you add `height: 300` instead of `flex: 1`?
+//       <View style={{
+//           flex: 1,
+//           flexDirection: 'row',
+//           justifyContent: 'center',
+//           alignItems: 'center'
+//       }}>
+//         <View style={{width: 100, height: 100, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+//         <View style={{width: 100, height: 100, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// }
 
 // export default class FixedDimensionsBasics extends Component {
 //   render() {
